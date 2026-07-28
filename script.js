aedb673e31a61d9703bcfd6bd0741135b141c5b2
Fathir Haririansyah <fathirhaririansyah@gmail.com>
2026-07-28 14:43:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function getWeather(){
         return;
     }
 
-    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const url=`https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric`;
 
     try{
 
